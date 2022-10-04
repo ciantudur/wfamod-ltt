ee184e6ff06b9b828d9f731aee4b1aa159cc7445
Cian Tudur <74679797+ciantudur@users.noreply.github.com>
2022-10-04 13:56:19 +0100

    Delete DESCRIPTION.R

--- DESCRIPTION.R
+++
@@ -1,9 +0,0 @@
-Package: wfamod-ltt
-Title: WFAMOD Land Transaction Tax Model
-Version: 1.0.1-beta
-Authors@R: 
-    person("Cian", "Sion", , "sionc1@cardiff.ac.uk", role = c("aut", "cre"),
-           comment = c(ORCID = "YOUR-ORCID-ID"))
-Description: A microsimulation model to forecast Land Transaction Tax revenue in Wales.
-License: `use_mit_license()`
-Encoding: UTF-8
